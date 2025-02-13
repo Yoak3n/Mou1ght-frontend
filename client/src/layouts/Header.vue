854276d2f60235cebb34ca1f36192f56ae02060a
Yoake <3354590676@qq.com>
2025-02-13 20:51:40 +0800

    ho213

--- client/src/layouts/Header.vue
+++ client/src/layouts/Header.vue
@@ -1,9 +1,11 @@
 <template>
     <div 
-    class="header w-full bg-sky-200 flex justify-between items-center h-12 pl-3 pr-3 fixed">
+    class="header w-full  flex justify-between items-center h-12 pl-3 pr-3 fixed">
         <div class="logo font-bold text-2xl caret-amber-100">Mou1ght</div>
         <div class="category">
-            <a href="/">首页</a>
+            <a href="/">
+                <img src="../assets/home.svg">
+            </a>
         </div>
         <div class="option">Option</div>
     </div>
@@ -15,11 +17,12 @@
 
 <style scoped>
 .header{
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
+    
+    transition: all 0.5s ease-in-out;
+    background-color: transparent;
     &:hover{
-        opacity: 1;
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+        background-color: #B8E6FE;
     }
 }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="home-wrapper">
-        <div class="lead h-100vh flex flex-col justify-center items-center">
+        <div class="lead h-screen flex flex-col justify-center items-center">
             <h1>欢迎来到我的博客</h1>
             <p>在这里，你可以分享你的故事，也可以倾听他人的故事。</p>
+        </div>
+        <div class="content">
+            <h2>最新文章</h2>
+            <div class="articles">
+                <div class="article" v-for="article in articles" :key="article.id">
+                    <h3>{{ article.title }}</h3>
+                    <p>{{ article.content }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,6 +22,10 @@
 
 <style scoped>
 .lead{
-    background-image: url();
+    background-image: url('https://img-baofun.zhhainiao.com/pcwallpaper_ugc/static/6a3c529ebda0178d42e72686c934ab6f.jpg?x-oss-process=image%2fresize%2cm_lfit%2cw_1920%2ch_1080');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-attachment: fixed;
+    background-size: cover;
 }
 </style>
